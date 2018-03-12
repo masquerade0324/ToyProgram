47a5e31205cf0f6d5bddb70791394349d97c8fbb
masquerade0324 <masquerade0324@gmail.com>
2018-03-13 01:56:51 +0900

    modify convert.py

--- convert.py
+++ convert.py
@@ -1,2 +1,10 @@
-import sys
-print(sys.version_info)
+import os
+
+path = "/write/your/path"
+dirs = os.listdir(path)
+
+for dir in dirs:
+    if len(dir) == 8 and dir.isdigit():
+        os.rename(path + "/" + dir, path + "/" + dir[0:4] + "-" + dir[4:6] + "-" + dir[6:8])
+
+print(os.listdir(path))
